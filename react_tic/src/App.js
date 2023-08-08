f3373605c6c712a70c77bd6ca677451543a9e915
Abhishek patil <abhishek3011patil@gmail.com>
2023-08-08 16:12:22 +0530

    added tic tac code environment and is running fine

--- react_tic/src/App.js
+++ react_tic/src/App.js
@@ -1,3 +1,42 @@
-export default function Square() {
-  return <button className="square">X</button>;
+
+import React, { Component }  from 'react';
+
+function Square({ value }) {
+  function handleClick() {
+    console.log('clicked!');
+  }
+
+  return (
+    <button
+      className="square"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
+}
+
+export default function Board() {
+  return (
+    <div>
+
+
+<div className="board-row">
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
+      </div>
+      <div className="board-row">
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
+      </div>
+      <div className="board-row">
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
+      </div>
+
+    </div>
+  );
 }
