a946a40979d8e8658d1de12e825a8f881b122649
Abhishek patil <abhishek3011patil@gmail.com>
2023-08-10 19:06:57 +0530

    did till collge theory

--- react_tic/src/App.js
+++ react_tic/src/App.js
@@ -1,48 +1,45 @@
-//completing game
+//Why immutability is important 
 
 import React, { Component }  from 'react';
-
-
 import { useState } from 'react';
 
-function Square() {
-  const [value, setValue] = useState(null);
+function Square({ value, onSquareClick }) {
 
 
-  function handleClick() {
-    setValue('X'); 
-  }
   
 
+  return <button className="square" onClick={onSquareClick}>{value}</button>;
 
-  return (
-    <button
-      className="square"
-      onClick={handleClick}
-    >
-      {value}
-    </button>
-  );
+
 }
 
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i) {
+    const nextSquares = squares.slice();
+    nextSquares[i] = "X";
+    setSquares(nextSquares);
+  }
   return (
     <div>
-      <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+     
+     <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} /> 
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square  />
-        <Square />
-        <Square />
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
+
     </div>
   );
 }
